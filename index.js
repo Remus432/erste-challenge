70c1628eaf4bd7930f73f7eb06fb2929e8e5b30f
remus432 <brosdev1@gmail.com>
2021-10-18 15:31:02 +0300

    first exercise

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world');
+const { readLine } = require("./utils");
+
+const pattern = /[^A-Za-z]+/
+const template = "Number of words: "
+
+readLine("Enter text:")
+  .then(userInput => {
+    const strArr = userInput.split(pattern)
+    const filteredStrArr = strArr.filter(str => str !== "")
+    console.log(template + filteredStrArr.length)
+  })
